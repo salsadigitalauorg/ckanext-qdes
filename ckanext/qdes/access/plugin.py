80066d57e10cecdaf650a6c31c773f96ab7e53da
Mark Calvert <markcalvert84@gmail.com>
2021-01-21 12:50:20 -0500

    Added IMiddleware interface to 'qdes_access' plugin Created 'QdesAuthMiddleware' to restrict access to only authenticated users excepts home, login, reset pages and asset's/api's

--- ckanext/qdes/access/plugin.py
+++ ckanext/qdes/access/plugin.py
@@ -1,11 +1,16 @@
+import logging
 import ckan.plugins as plugins
-from ckanext.qdes.access import blueprint
+
+from ckanext.qdes.access import blueprint, middleware
 from ckanext.qdes.access.logic.auth import update as auth_update
+
+log = logging.getLogger(__name__)
 
 
 class QdesAccessPlugin(plugins.SingletonPlugin):
     plugins.implements(plugins.IBlueprint)
     plugins.implements(plugins.IAuthFunctions)
+    plugins.implements(plugins.IMiddleware, inherit=True)
 
     # IBlueprint
     def get_blueprint(self):
@@ -16,3 +21,7 @@ class QdesAccessPlugin(plugins.SingletonPlugin):
         return {
             'user_update': auth_update.user_update,
         }
+
+    # IMiddleware
+    def make_middleware(self, app, config):
+        return middleware.QdesAuthMiddleware(app, config)
