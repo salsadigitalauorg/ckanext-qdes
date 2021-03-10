eda016622763e66c2fbe93dbc4782a6c4608f7ad
Mark Calvert <markcalvert84@gmail.com>
2021-03-10 10:45:42 -0700

    [DDCI-650] - Added new blueprint route '/service/login' the will display the default ckan login page - Updated middleware to use CKAN config values for 'unauthorized_pages_allowed' and 'unauthorized_redirect_location' - Updated middleware to use 302 redirect status code to use Location header - Added auth overrides for 'user_reset' & 'request_reset' to have no access - Updated header.html template to only display 'header_site_navigation' & 'header_site_search' & 'header_site_navigation_desktop' if user is authenticated - Added login.html template override to not display user register and forgotten password sections

--- ckanext/qdes/access/middleware.py
+++ ckanext/qdes/access/middleware.py
@@ -15,23 +15,28 @@ class QdesAuthMiddleware(object):
     def __call__(self, environ, start_response):
         # if logged in via browser cookies or API key, all pages accessible
         if 'repoze.who.identity' not in environ and not self._get_user_for_apikey(environ):
-            # The only pages unauthenticated users have access to are below
-            # login, password reset, saml2login, acs (SAML Assertion Consumer Service) and user unauthorised
-            # But still allow unauthenticated access to assets and API
+            # The only pages unauthorized users have access to are below
+            # service login, saml2login, acs (SAML Assertion Consumer Service), user unauthorised and logout pages
+            # But still allow unauthorized access to assets and API
             # API authentication will be handled by API auth/actions but some have public access
-            if (environ['PATH_INFO'] not in ['/user/login', '/user/saml2login', '/acs', '/user/unauthorised'] \
-                and not environ['PATH_INFO'].startswith('/user/reset')) \
-                and not environ['PATH_INFO'].startswith('/base') \
-                and not environ['PATH_INFO'].startswith('/api') \
-                and not environ['PATH_INFO'].startswith('/webassets') \
-                and not environ['PATH_INFO'].startswith('/images') \
-                and not environ['PATH_INFO'].startswith('/css') \
-                and not environ['PATH_INFO'].startswith('/js') \
-                and not environ['PATH_INFO'].startswith('/_debug') \
-                and not environ['PATH_INFO'].startswith('/uploads'):
-                status = "401 Unauthorized"
-                headers = [('Location', '/user.login'),
+            unauthorized_pages_allowed = toolkit.aslist(toolkit.config.get('ckanext.qdes_access.unauthorized_pages_allowed', ''))
+            if environ['PATH_INFO'] not in unauthorized_pages_allowed \
+                    and not environ['PATH_INFO'].startswith('/base') \
+                    and not environ['PATH_INFO'].startswith('/api') \
+                    and not environ['PATH_INFO'].startswith('/webassets') \
+                    and not environ['PATH_INFO'].startswith('/images') \
+                    and not environ['PATH_INFO'].startswith('/css') \
+                    and not environ['PATH_INFO'].startswith('/js') \
+                    and not environ['PATH_INFO'].startswith('/_debug') \
+                    and not environ['PATH_INFO'].startswith('/uploads'):
+
+                log.debug(f"Unauthorized page accessed: {environ['PATH_INFO']}")
+                # Status code needs to be 3xx (redirection) for Location header to be used
+                status = "302 Unauthorized"
+                location = toolkit.config.get('ckanext.qdes_access.unauthorized_redirect_location', '/user/saml2login')
+                headers = [('Location', location),
                            ('Content-Length', '0')]
+                log.debug(f"Redirecting to: {location}")
                 start_response(status, headers)
                 # Return now as we want to end the request
                 return []
