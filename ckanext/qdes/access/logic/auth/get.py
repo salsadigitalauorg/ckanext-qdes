079c48987c45e11d49020c2f43eb09028e35dea1
Konstantin Sivakov <konstantin.sivakov@gmail.com>
2021-05-19 01:10:36 +0200

    [DDCI-869] Remove api_token_list overridden method (#38)
    * Remove api_token_list overriden method
    
    * Remove commented code

--- ckanext/qdes/access/logic/auth/get.py
+++ ckanext/qdes/access/logic/auth/get.py
@@ -1,12 +1,6 @@
 import ckan.plugins.toolkit as toolkit
 
 
-@toolkit.chained_auth_function
-def api_token_list(next_auth, context, data_dict):
-    # sysadmins only
-    return {'success': False}
-
-
 @toolkit.auth_sysadmins_check
 def user_reset(context, data_dict):
     # No access
