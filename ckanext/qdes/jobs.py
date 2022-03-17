4c9a5a94046a328eee679e9230c2779304b22cde
Konstantin Sivakov <konstantin.sivakov@gmail.com>
2022-03-17 22:47:22 +0100

    Check if ENV variable exists

--- ckanext/qdes/jobs.py
+++ ckanext/qdes/jobs.py
@@ -150,8 +150,9 @@ def mark_as_reviewed(datasets):
 def ckan_worker_job_monitor():
     try:
         log.info(f'Sending notification to healthchecks for CKAN worker job monitor')
-        requests.get(os.environ.get('MONITOR_QDES_JOBWORKER'), timeout=10)
-        log.info(f'Successfully sent notification to healthchecks for CKAN worker job monitor')
+        if os.environ.get('MONITOR_QDES_JOBWORKER', None):
+            requests.get(os.environ.get('MONITOR_QDES_JOBWORKER'), timeout=10)
+            log.info(f'Successfully sent notification to healthchecks for CKAN worker job monitor')
     except requests.RequestException as e:
         log.error(f'Failed to send ckan worker job monitor notification to healthchecks')
         log.error(str(e))
