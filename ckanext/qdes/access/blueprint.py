a36524a56fee120f43df7eeac19c85320cb75cca
MarkCalvert <37602611+MarkCalvert@users.noreply.github.com>
2022-11-14 15:14:14 -0500

    Migrated qdes code from Saml2Auth into this extension (#26)
    * Migrated qdes code from Saml2Auth into this extension
    Using the Saml2Auth interfaces to apply qdes custom logic and to update user organisations
    
    * Added qdes identity provider metadata xml
    
    * Cleaned up imports
    
    * Refactored logic foy sysadmins

--- ckanext/qdes/access/blueprint.py
+++ ckanext/qdes/access/blueprint.py
@@ -1,11 +1,23 @@
 import logging
-
-import ckan.plugins.toolkit as toolkit
 import ckan.views.user as ckan_view_user
+
+from ckan.plugins.toolkit import request, render
 from flask import Blueprint
 
 log = logging.getLogger(__name__)
 qdes_access = Blueprint('qdes_access', __name__)
 
 
+def unauthorised():
+    fullname = request.params.get('fullname', '')
+    email = request.params.get('email', '')
+    extra_vars = {
+        u'code': [403],
+        u'name': u'Not Authorised',
+        u'content': u' User {0} with email {1} is not a member of any authenticated AD group'.format(fullname, email)
+    }
+    return render(u'error_document_template.html', extra_vars)
+
+
 qdes_access.add_url_rule(u'/service/login', view_func=ckan_view_user.login)
+qdes_access.add_url_rule(u'/user/unauthorised', view_func=unauthorised)
